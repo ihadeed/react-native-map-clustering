42e98682045c2de8912c8045fe41b11779f257a4
Ibby Hadeed <ibby@zyra.ca>
2018-10-03 17:57:38 -0400

    Cleanup

--- MapView/MapWithClustering.js
+++ MapView/MapWithClustering.js
@@ -1,20 +1,58 @@
 import React, { Component } from 'react';
+import { Dimensions } from 'react-native';
 import PropTypes from 'prop-types';
 import MapView from 'react-native-maps';
 import { width as w, height as h } from 'react-native-dimension';
 import SuperCluster from 'supercluster';
 import CustomMarker from './CustomMarker';
 
+const calculateBBox = region => [
+    region.longitude - region.longitudeDelta, // westLng - min lng
+    region.latitude - region.latitudeDelta, // southLat - min lat
+    region.longitude + region.longitudeDelta , // eastLng - max lng
+    region.latitude + region.latitudeDelta// northLat - max lat
+];
+
+const getBoundsZoomLevel = (bounds, mapDim) => {
+    const WORLD_DIM = { height: mapDim.height, width: mapDim.width };
+    const ZOOM_MAX = 20;
+
+    function latRad(lat) {
+        const sin = Math.sin(lat * Math.PI / 180);
+        const radX2 = Math.log((1 + sin) / (1 - sin)) / 2;
+        return Math.max(Math.min(radX2, Math.PI), -Math.PI) / 2;
+    }
+
+    function zoom(mapPx, worldPx, fraction) {
+        return Math.floor(Math.log(mapPx / worldPx / fraction) / Math.LN2);
+    }
+
+    const latFraction = (latRad(bounds[3]) - latRad(bounds[1])) / Math.PI;
+    const lngDiff = bounds[2] - bounds[0];
+    const lngFraction = ((lngDiff < 0) ? (lngDiff + 360) : lngDiff) / 360;
+    const latZoom = zoom(mapDim.height, WORLD_DIM.height, latFraction);
+    const lngZoom = zoom(mapDim.width, WORLD_DIM.width, lngFraction);
+
+    return Math.min(latZoom, lngZoom, ZOOM_MAX);
+};
+
+const removeChildrenFromProps = (props) => ({
+    ...props,
+    children: undefined,
+});
+
+const CLUSTER_SIZE = Dimensions.get('window').width * 0.15;
+
 export default class MapWithClustering extends Component {
   state = {
     currentRegion: this.props.region,
     clusterStyle: {
-      borderRadius: w(15),
+      borderRadius: CLUSTER_SIZE,
       backgroundColor: this.props.clusterColor,
       borderColor: this.props.clusterBorderColor,
       borderWidth: this.props.clusterBorderWidth,
-      width: w(15),
-      height: w(15),
+      width: CLUSTER_SIZE,
+      height: CLUSTER_SIZE,
       justifyContent: 'center',
       alignItems: 'center',
     },
@@ -83,42 +121,12 @@ export default class MapWithClustering extends Component {
     });
   };
 
-  calculateBBox = region => [
-    region.longitude - region.longitudeDelta, // westLng - min lng
-    region.latitude - region.latitudeDelta, // southLat - min lat
-    region.longitude + region.longitudeDelta , // eastLng - max lng
-    region.latitude + region.latitudeDelta// northLat - max lat
-  ];
-
-  getBoundsZoomLevel = (bounds, mapDim) => {
-    const WORLD_DIM = { height: mapDim.height, width: mapDim.width };
-    const ZOOM_MAX = 20;
-
-    function latRad(lat) {
-      const sin = Math.sin(lat * Math.PI / 180);
-      const radX2 = Math.log((1 + sin) / (1 - sin)) / 2;
-      return Math.max(Math.min(radX2, Math.PI), -Math.PI) / 2;
-    }
-
-    function zoom(mapPx, worldPx, fraction) {
-      return Math.floor(Math.log(mapPx / worldPx / fraction) / Math.LN2);
-    }
-
-    const latFraction = (latRad(bounds[3]) - latRad(bounds[1])) / Math.PI;
-    const lngDiff = bounds[2] - bounds[0];
-    const lngFraction = ((lngDiff < 0) ? (lngDiff + 360) : lngDiff) / 360;
-    const latZoom = zoom(mapDim.height, WORLD_DIM.height, latFraction);
-    const lngZoom = zoom(mapDim.width, WORLD_DIM.width, lngFraction);
-
-    return Math.min(latZoom, lngZoom, ZOOM_MAX);
-  };
-
   calculateClustersForMap = async (currentRegion = this.state.currentRegion) => {
     let clusteredMarkers = [];
 
     if (this.props.clustering && this.superCluster) {
-      const bBox = this.calculateBBox(this.state.currentRegion);
-      let zoom = this.getBoundsZoomLevel(bBox, { height: h(100), width: w(100) });
+      const bBox = calculateBBox(this.state.currentRegion);
+      let zoom = getBoundsZoomLevel(bBox, { height: h(100), width: w(100) });
       const clusters = await this.superCluster.getClusters([bBox[0], bBox[1], bBox[2], bBox[3]], zoom);
 
       clusteredMarkers = clusters.map(cluster => (<CustomMarker
@@ -141,15 +149,11 @@ export default class MapWithClustering extends Component {
     });
   };
 
-  removeChildrenFromProps = (props) => ({
-      ...props,
-      children: undefined,
-  });
 
   render() {
     return (
       <MapView
-        {...this.removeChildrenFromProps(this.props)}
+        {...removeChildrenFromProps(this.props)}
         ref={(ref) => { this.root = ref; }}
         region={this.state.currentRegion}
         onRegionChangeComplete={this.onRegionChangeComplete}
